38e8ca469070490784d2de2277fcc40691325451
tooooo1 <wer4272@nate.com>
2021-04-08 17:39:44 +0900

    study

--- study/Pro_1.java
+++ study/Pro_1.java
@@ -1,16 +1,31 @@
 package study;
 
+import java.util.Scanner;
+
 class Solution {
+    private String phone_number;
     public String solution(String phone_number) {
         String answer = "";
+        char[] change = phone_number.toCharArray();
+        for(int i =0 ; phone_number.length()-4>i;i++)
+            change[i]='*';
+        answer = String.valueOf(change);
         return answer;
     }
 }
 
 public class Pro_1 {
     public static void main(String[] args) {
-        String[] phone_number = new String[5];
-
+        String phone_number;
+        Scanner in = new Scanner(System.in);
+        phone_number = in.nextLine();
+        Solution test = new Solution();
+        if ((phone_number.length()>=4) && (phone_number.length()<=20))
+            System.out.println(test.solution(phone_number));
+        else
+            System.out.println("길이가 4 미만이거나 20을 초과하였습니다.");
     }
+
+
 
 }
