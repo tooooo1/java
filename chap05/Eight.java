169182aac468bbd8fb4fab3e9b2c947a2a617079
tooooo1 <wer4272@nate.com>
2021-04-10 23:18:22 +0900

    study

---
+++ chap05/Eight.java
@@ -0,0 +1,17 @@
+package chap05;
+
+
+public class Eight {
+    public static void main(String[] args) {
+
+
+    }
+
+    public void Program(int m, int n, double p) {
+        int[][] b= new int[m][n];
+        p = Math.random();
+
+
+    }
+
+}
