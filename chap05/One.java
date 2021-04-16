b7228ac322a47385fa5ea2cc4946d012cf6671f5
tooooo1 <wer4272@nate.com>
2021-04-17 05:16:01 +0900

    study

--- chap05/One.java
+++ chap05/One.java
@@ -3,22 +3,22 @@ package chap05;
 import java.util.Scanner;
 
 public class One {
-    static int num =0;
-    static int countChar (String s,char c) {
-        for(int i=0; i < s.length(); i++) {
-            if (s.charAt(i)==c)
-                num++;
-        }
-        return num;
-    }
+    static int count =0;
+
     public static void main(String[] args) {
-        System.out.println("문자열과 문자를 입력하시오.");
         Scanner in = new Scanner(System.in);
         String s = in.next();
         char c = in.next().charAt(0);
 
-        System.out.println(s + " 에는 " + c + "가(이)" + countChar(s,c) + " 개가 있습니다.");
+        System.out.println(countChar(s,c));
     }
 
-
+    //countChar 메서드
+    static int countChar (String s,char c) {
+        for(int i=0; i < s.length(); i++) {
+            if (s.charAt(i)==c)
+                count++;
+        }
+        return count;
+    }
 }
