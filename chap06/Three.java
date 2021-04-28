b8658e2a6c3368ffc298125e75fc429c507e235e
tooooo1 <wer4272@nate.com>
2021-04-28 09:11:35 +0900

    study

---
+++ chap06/Three.java
@@ -0,0 +1,54 @@
+package chap06;
+
+class Point {
+    private int x,y;
+    Point(int x, int y) {
+        this.x=x;
+        this.y=y;
+    }
+    int getX() { //접근자
+        return x;
+    }
+    int getY() {
+        return x;
+    }
+    void setX(int x) { //생성자
+        this.x = x;
+    }
+    void setY(int y) {
+        this.y = y;
+    }
+    @Override
+    public String toString() {
+        return "Point [x=" + x + ", y=" + y + "]";
+    }
+}
+
+class MovablePoint extends Point {
+    private int xSpeed, ySpeed;
+    MovablePoint(int x, int y,int xSpeed, int ySpeed) {
+        super(x,y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+    int getxSpeed() { //접근자
+        return xSpeed;
+    }
+    int getySpeed() {
+        return ySpeed;
+    }
+    void setxSpeed(int xSpeed) { //생성자
+        this.xSpeed = xSpeed;
+    }
+    void setySpeed(int ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+    @Override
+    public String toString() {
+        return "MovablePoint [x=" + getX() + ", y =" +getY() + " xSpeed=" + xSpeed + ", ySpeed =" + ySpeed + "]";
+    }
+}
+
+
+public class Three {
+}
