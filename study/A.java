8ca37a0c7ecac54b3f1dd8bafbaf84f0de772a4b
tooooo1 <wer4272@nate.com>
2021-07-02 22:27:10 +0900

    study

---
+++ study/A.java
@@ -0,0 +1,10 @@
+
+public class A {
+    public static void main(String[] args) {
+        int a=1, b=2; switch(a++) { case 1: b+=10; case 2: b+=5; case 3: b+=2;}
+
+        System.out.println(b);
+    }
+
+
+}
